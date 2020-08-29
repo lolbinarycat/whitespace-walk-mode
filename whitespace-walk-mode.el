9e5c0943a48d6a803bf1ed838a1d55b6f3e37af3
binary cat <dogedoge61@gmail.com>
2020-08-29 11:08:06 -0400

    mode now functional

--- whitespace-walk-mode.el
+++ whitespace-walk-mode.el
@@ -1,12 +1,19 @@
 (defun left-char-over-whitespace ()
   (interactive)
-  (let (pchar (preceding-char))
-    (left-char)
-    (if (= pchar 32)
-        (left-char-over-whitespace))))
+  (left-char)
+  (if (= 32 (char-before))
+      (left-char-over-whitespace)))
 
-
+(defun right-char-over-whitespace ()
+  (interactive)
+  (right-char)
+  (if (= 32 (char-after))
+      (right-char-over-whitespace)))
+  
 
 (define-minor-mode whitespace-walk-mode
   "move over whitespace as if it wasn't there."
-  :lighter " SpW")     
+  :lighter " SpW"
+  :keymap (make-sparse-keymap)
+  (define-key whitespace-walk-mode-map (kbd "<left>") 'left-char-over-whitespace)
+  (define-key whitespace-walk-mode-map (kbd "<right>") 'right-char-over-whitespace))
