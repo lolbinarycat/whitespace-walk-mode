142aa7609b8414c1b83794a2ea59931c0b13dc7c
binary cat <dogedoge61@gmail.com>
2020-08-30 12:16:58 -0400

    added skipping over tabs and way to add more charachters to skip over

--- whitespace-walk-mode.el
+++ whitespace-walk-mode.el
@@ -1,19 +1,21 @@
 (defun left-char-over-whitespace ()
   (interactive)
   (left-char)
-  (if (= 32 (char-before))
+  (if (member (char-before) whitespace-walk-spacechars)
       (left-char-over-whitespace)))
 
 (defun right-char-over-whitespace ()
   (interactive)
   (right-char)
-  (if (= 32 (char-after))
-      (right-char-over-whitespace)))
-  
-
+  (if (member (char-after) whitespace-walk-spacechars)
+       (right-char-over-whitespace)))
+    
 (define-minor-mode whitespace-walk-mode
   "move over whitespace as if it wasn't there."
   :lighter " SpW"
   :keymap (make-sparse-keymap)
   (define-key whitespace-walk-mode-map (kbd "<left>") 'left-char-over-whitespace)
-  (define-key whitespace-walk-mode-map (kbd "<right>") 'right-char-over-whitespace))
+  (define-key whitespace-walk-mode-map (kbd "<right>") 'right-char-over-whitespace)
+  (make-variable-buffer-local
+   (defvar whitespace-walk-spacechars '(32 9)
+     "List of charachters to skip over.")))
